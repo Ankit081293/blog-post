7aaf9eb5552957cc920e34f4bb7fd078a37bbd6e
Ankit Kachhadiya <akachhadiya@toastmasters.org>
2025-04-17 16:43:27 +0530

    added message for feature 2 :wq

--- src/components/About.js
+++ src/components/About.js
@@ -7,6 +7,7 @@ function About() {
   return (   
     <div className="container p-4">
       <h2>React Redux Feature</h2>
+      <h2>React Redux Feature 2</h2>
       <h2 className="mb-4">Todo App</h2>
       <div>
           <AddToDo/>
